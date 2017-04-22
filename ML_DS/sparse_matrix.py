69c8d492fef53c2e3388449a38094ae8b6677eea
Zhifeng Deng <zhdeng@linkedin.com>
2017-04-22 12:11:49 -0700

    sparse vector and matrix

---
+++ ML_DS/sparse_matrix.py
@@ -0,0 +1,57 @@
+import collections
+
+class SparseVector(object):
+    def __init__(self, kv_list):
+        self.kv_map = dict(kv_list)
+
+    def __str__(self):
+        return str(self.kv_map)
+
+    def __getitem__(self, k):
+        return self.kv_map[k]
+
+    def __mul__(self, other):
+        result = 0
+        for k in self.kv_map.keys():
+            if k in other.kv_map.keys():
+                result += self[k] * other[k]
+
+        return result
+
+class SparseMatrix(object):
+    def __init__(self, kv_list):
+        self.kv_map = dict(kv_list)
+
+    def __str__(self):
+        return str(self.kv_map)
+
+    def __getitem__(self, k):
+        return self.kv_map[k]
+
+    def __mul__(self, other):
+        result = collections.defaultdict(int)
+
+        for (x1, y1) in self.kv_map.keys():
+            for (x2, y2)in other.kv_map.keys():
+                if y1 == x2:
+                    result[(x1, y2)] += self[(x1, y1)] * other[(x2, y2)]
+        return SparseMatrix(result.items())
+
+def test():
+    a = SparseVector([(0, 1), (9, 2)])
+    b = SparseVector([(0, 3), (9, 1)])
+
+    print a
+    print a*b
+
+    ma = SparseMatrix([((0, 0), 1), ((0, 1), 2), ((0, 2), 3)])
+    print ma
+    mb = SparseMatrix([((0, 0), 1), ((1, 0), 2), ((2, 0), 3)])
+    print mb
+
+    mc = ma * mb
+    print mc
+
+
+test()
+
